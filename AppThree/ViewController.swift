7e4359c80a13e153e37d82692564bd801d2cabf2
Arseny <sinica2003@inbox.ru>
2023-10-29 22:01:13 -0400

    Initial Commit

---
+++ AppThree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppThree
+//
+//  Created by Арсений on 10/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
